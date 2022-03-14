6f30f9d516c896f9e6dfe1198125db2e1a7af3c6
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-03-14 23:23:26 +0200

    Anime Poster under the Stats Bar
    styled the status container, added border radius on the Stats block,

--- anix/src/App.js
+++ anix/src/App.js
@@ -1,10 +1,12 @@
 import Navigation from "./Components/Navigation";
 import Stats from "./Components/Stats";
+import MainInfo from "./Components/MainInfo";
 import {Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <MainInfo />
       <Routes>
         <Route path="/index" element={ <Stats />}/>
       </Routes>
---
+++ anix/src/Components/MainInfo.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+axios.get("https://api.jikan.moe/v4/anime/20")
+.then((response)=>{
+    console.log(response)
+})
+.catch((err)=>{
+    console.log(err)
+});
+const MainInfo =()=>{
+    return(
+        <div className='mainInfo-container'>
+            <div className="poster"></div>
+            <div className="title"></div>
+            <div className="synopsis"></div>
+        </div>
+    );
+}
+export default MainInfo
