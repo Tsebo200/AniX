d74b717201de78021d8ace701acd31f9c4037ad8
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-03-26 20:44:58 +0200

    Added Logos in the file

--- anix/src/App.js
+++ anix/src/App.js
@@ -1,12 +1,12 @@
-import Navigation from "./Components/Navigation";
-import Stats from "./Components/Stats";
-import MainInfo from "./Components/MainInfo";
+import Navigation from "./components/Navigation";
+import Stats from "./components/Stats";
+import MainInfo from "./components/MainInfo";
 import {Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <MainInfo />
+      < MainInfo />
       <Routes>
         <Route path="/index" element={ <Stats />}/>
       </Routes>
