3ce4c397e608042a46e96d599fe28acf6fba7d1f
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-04-03 01:03:51 +0200

    Added 2 Doughnut graphs
    Styled the doughnut graphs and added a dropdown, just need to make it all dynamic

--- anix/src/components/Comp.js
+++ anix/src/components/Comp.js
@@ -2,11 +2,11 @@ import 'chart.js/auto';
 import React from 'react';
 import axios from 'axios';
 import { Bar } from 'react-chartjs-2';
-import { Pie } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 
 axios.get("https://api.jikan.moe/v4/anime")
 .then((res)=>{
-    
+    let name = res.data.data[0].title
 })
 
 const Comp = ()=>{
@@ -15,17 +15,80 @@ const Comp = ()=>{
         
         <>
         <div className="main-container">
-        <div className='dropdown'>
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a className="dropdown-item" href="/compare">Action</a></li>
-    <li><a className="dropdown-item" href="/compare">Another action</a></li>
-    <li><a className="dropdown-item" href="/compare">Something else here</a></li>
-  </ul>
-</div>
-        <div className="componentInterior">
+            <h1 className='compare-heading'>Choose An Anime To Compare</h1>
+        <div className='dropdown-one'>
+            <form>
+                <select name="Animes">
+                    <option value="Naruto">Naruto</option>
+                    <option value="One Piece">One Piece</option>
+                    <option value="Cowboy Bebop">CowBoy Bebop</option>
+                    <option value="Trigun">Trigun</option>
+                    <option value="Bouken Ou Beet">Bouken Ou Beet</option>
+                    <option value="Initial D">Initial D</option>
+                </select>
+                <br></br>
+                <input className='compare-btn' type="submit" value="Compare"></input>
+            </form>
+        </div>
+        <h2 className='vs'>vs</h2>
+        <div className='dropdown-two'>
+            <form>
+                <select name="Animes">
+                    <option value="Naruto">Naruto</option>
+                    <option value="One Piece">One Piece</option>
+                    <option value="Cowboy Bebop">CowBoy Bebop</option>
+                    <option value="Trigun">Trigun</option>
+                    <option value="Bouken Ou Beet">Bouken Ou Beet</option>
+                    <option value="Initial D">Initial D</option>
+                </select>
+            </form>
+        </div>
+
+        <div className='doughnutGraph-one'>
+        <h1 className='table-heading'>Naruto</h1>
+        <Doughnut data = {{
+        labels: ['Watching','Completed','Dropped','Plan To Watch'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 15, 18],
+            backgroundColor: [
+                '#CED6E0',
+                '#FF4757',
+                '#7D8897',
+                '#FF6B81',
+            ],
+            borderColor: [
+                
+            ],
+            borderWidth: 1
+        }]
+    }}
+    />
+
+    <div className='doughnutGraph-two'>
+    <h1 className='table-heading'>One Piece</h1>
+        <Doughnut data = {{
+        labels: ['Watching','Completed','Dropped','Plan To Watch'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 15, 18],
+            backgroundColor: [
+                '#CED6E0',
+                '#FF4757',
+                '#7D8897',
+                '#FF6B81',
+            ],
+            borderColor: [
+                
+            ],
+            borderWidth: 1
+        }]
+    }}
+    />
+    </div>
+        </div>
+
+        <div className="barGraph">
             <h1 className='table-heading'>Users Favourites</h1>
         <Bar 
             data={{
